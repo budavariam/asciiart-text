17f1386b8eafb412007a10f831b8032338e58a37
Mátyás Budavári <budavariam@gmail.com>
2021-02-22 21:20:03 +0100

    Add width selector

--- src/App.js
+++ src/App.js
@@ -42,10 +42,12 @@ const FigletContainer = (props) => {
 
 function MultiApp() {
   const [text, setText] = useState("Type Something")
+  const [width, setWidth] = useState(80)
   return (
     <div className="App">
+      <input type="number" min={0} max={3000} value={width} onChange={(e) => { setWidth(e.target.value) }} />
       <textarea value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
-      {Object.values(fonts).map((font) => <FigletContainer key={font.name} text={text} font={font.name} fontData={font.value} />)}
+      {Object.values(fonts).map((font) => <FigletContainer key={font.name} text={text} font={font.name} fontData={font.value} width={width} />)}
     </div>
   );
 }
